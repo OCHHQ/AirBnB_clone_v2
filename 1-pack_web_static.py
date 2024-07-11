798654d8585df2fb6dc8626b9fef295f0f9caf7c
OCHHQ <enosejecollins@gmail.com>
2024-07-11 06:16:10 -0700

    1. Compress before sending

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,13 +1,16 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """
-Fabric script that generates a .tgz archive from the contents of the web_static folder
+Packs the web_static content into a .tgz file
 """
+
 from fabric.api import local
 from datetime import datetime
 import os
 
 def do_pack():
-    """Packs the web_static content into a .tgz file"""
+    """
+    Packs the web_static content into a .tgz file
+    """
     now = datetime.now()
     archive_name = "web_static_{}{}{}{}{}{}.tgz".format(
         now.year, now.month, now.day, now.hour, now.minute, now.second)
