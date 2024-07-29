2ce982aed6727381217d393bf2bcf979f541b10a
Collins Enoseje <enosejecollins@gmail.com>
2024-07-29 18:09:48 +0100

    Add Task 2: C is fun!

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,33 +1,34 @@
 #!/usr/bin/python3
-"""
-A simple Flask web application
-"""
 
+"""
+This module contains a simple Flask web application.
+"""
 
 from flask import Flask
 
-
 app = Flask(__name__)
-
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
-    """Displays 'Hello HBNB!'"""
+    """
+    Displays 'Hello HBNB!' on the root route.
+    """
     return "Hello HBNB!"
-
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """Displays 'HBNB'"""
+    """
+    Displays 'HBNB' on the /hbnb route.
+    """
     return "HBNB"
-
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_text(text):
-    """Displays 'C ' followed by the value of the text var 
-    (replace underscore _ symbols with a space)"""
+    """
+    Displays 'C ' followed by the value of the text variable, 
+    replacing underscore _ symbols with a space.
+    """
     return "C {}".format(text.replace('_', ' '))
-
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
